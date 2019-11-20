3c89391ce1e5987df049f3d401f9e553b9674e67
Alex Bodea <alexandru.bodea@spyhce.com>
2019-11-20 11:32:21 +0200

    Second com

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
-import os
-
+import subprocess
 
 print('Hello word')
 
 
-def shell(self, command: str, output: bool = False):
+def shell(command: str, output: bool = False):
     try:
         if not output:
-            os.subprocess.call([command], shell=True)
+            subprocess.call([command], shell=True)
         else:
-            return os.subprocess.check_output([command], shell=True).decode().strip()
-    except os.subprocess.CalledProcessError as e:
+            return subprocess.check_output([command], shell=True).decode().strip()
+    except subprocess.CalledProcessError as e:
         raise ValueError('Error') from e
 
-v = shell()
+
+v = shell('git describe --tags --long --dirty --always')
 print(v)
